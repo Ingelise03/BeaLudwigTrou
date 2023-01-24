9b0dc483c4669d958c7d12d204de5f857a6d9bb5
Ingelise03 <ingelisewessels03@gmail.com>
2023-01-24 20:41:01 +0200

    Changes to be committed: 	modified:   bealudwigtrou/src/pages/index.js Changes not staged for commit: 	deleted:    bealudwigtrou/src/pages/components/footer.js 	deleted:    bealudwigtrou/src/pages/components/header.js 	deleted:    bealudwigtrou/src/pages/components/index.js 	deleted:    bealudwigtrou/src/pages/components/main.js 	deleted:    bealudwigtrou/src/pages/components/rsvp.js
    Untracked files:
    	bealudwigtrou/src/components/

--- bealudwigtrou/src/pages/index.js
+++ bealudwigtrou/src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Header, MainPart, Footer, Rsvp } from './components'
+import { Header, MainPart, Footer, Rsvp } from '../components'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
